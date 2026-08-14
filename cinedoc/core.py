a32ef54e93754b267becb10668e5f4a84e1e53d8
imraunak1910 <raunakraj1910@gmail.com>
2026-08-15 01:19:32 +0530

    Template Prompting

---
+++ cinedoc/core.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_mistralai import ChatMistralAI
+
+model=ChatMistralAI(model="mistral-small-2603")
+
+response=model.invoke("what is an LLM?")
+
+print(response.content)
+
